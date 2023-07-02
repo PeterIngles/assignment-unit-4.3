b506aabf4d8366290115790203cb6f2a782394d2
Peter Ingles <peter.ingles@gmail.com>
2023-07-02 14:09:41 -0500

    Finished Assignment 4.3

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,16 +2,32 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+let basket = []
 
+function addItem(item) {
+    basket.push(item);
+    console.log(item + " added to the basket")
+    return true;
+}
 
+function listItems() {
+    if (basket.length === 0) {
+      console.log('The basket is empty.');
+    } else {
+      for ( i = 0; i < basket.length; i++) {
+        console.log(basket[i]);
+      }
+    }
+  }
 
+function empty() {
+    basket = [];
+}
 
-
-
-
-
-
-
+console.log('Adding to the basket:');
+addItem('Apples');
+addItem('Steak');
+addItem('Cheese');
 
 // DO NOT MODIFY
 // Used for automated testing
@@ -25,6 +41,6 @@ try {
         isFull: typeof isFull !== 'undefined' ? isFull : undefined,
         removeItem: typeof removeItem !== 'undefined' ? removeItem : undefined,
     };
-} catch(e) {
+} catch (e) {
     // Do nothing
 }
